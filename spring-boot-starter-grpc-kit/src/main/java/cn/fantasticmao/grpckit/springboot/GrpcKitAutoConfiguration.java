ce979d2d4c170ce4450372260c6284904de38dd6
Harrison <yuanlu_k@163.com>
2022-08-12 14:12:23 +0800

    add comment

--- spring-boot-starter-grpc-kit/src/main/java/cn/fantasticmao/grpckit/springboot/GrpcKitAutoConfiguration.java
+++ spring-boot-starter-grpc-kit/src/main/java/cn/fantasticmao/grpckit/springboot/GrpcKitAutoConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 /**
  * gRPC Kit Auto Configuration.
  *
+ * 基于Java的容器配置
+ *
  * @author fantasticmao
  * @version 1.39.0
  * @see <a href="https://docs.spring.io/spring-boot/docs/2.5.1/reference/html/features.html#features.developing-auto-configuration">Creating Your Own Auto-configuration</a>
@@ -29,6 +31,12 @@ public class GrpcKitAutoConfiguration {
         return GrpcKitConfig.loadAndParse(configPath);
     }
 
+    /**
+     * 注入GrpcStubBeanPostProcessor，且依赖GrpcKitConfig
+     *
+     * @param config
+     * @return
+     */
     @Bean
     @ConditionalOnMissingBean
     public GrpcStubBeanPostProcessor grpcStubBeanPostProcessor(@Autowired GrpcKitConfig config) {
